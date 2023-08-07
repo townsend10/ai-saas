d15374d81b690545de13042373c5af5e50a4a268
george michael <jogos_townsend@hotmail.com>
2023-08-07 11:13:58 -0300

    testando commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button />
+      <Button /> x
     </div>
   );
 };
