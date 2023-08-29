2b7d57896a17b71609cb1ae6608363019e2b774f
george michael <jogos_townsend@hotmail.com>
2023-08-29 11:42:08 -0300

    finisging functionalites and add prisma

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -1,26 +1,23 @@
 "use client";
-import axios from "axios";
+import Empty from "@/components/empty";
 import Heading from "@/components/heading";
-import { MessageSquare } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { formSchema } from "./constants";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
+import Loader from "@/components/loader";
+import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Music } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { ChatCompletionRequestMessage } from "openai";
-import Empty from "@/components/empty";
-import Loader from "@/components/loader";
-import { cn } from "@/lib/utils";
-import UserAvatar from "@/components/user-avatar";
-import BotAvatar from "@/components/bot-avatar";
-const ConversationPage = () => {
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { formSchema } from "./constants";
+const MusicPage = () => {
   const router = useRouter();
 
-  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+  const [music, setMusic] = useState<string>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -33,17 +30,11 @@ const ConversationPage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const userMessage: ChatCompletionRequestMessage = {
-        role: "user",
-        content: values.prompt,
-      };
-      const newMessages = [...messages, userMessage];
+      setMusic(undefined);
 
-      const response = await axios.post("/api/conversation", {
-        messages: newMessages,
-      });
-      setMessages((current) => [...current, userMessage, response.data]);
+      const response = await axios.post("/api/music", values);
 
+      setMusic(response.data.audio);
       form.reset();
     } catch (error: any) {
       //todo
@@ -54,11 +45,11 @@ const ConversationPage = () => {
   return (
     <div>
       <Heading
-        title="Conversation"
-        description="Our most advanced conversation"
-        icon={MessageSquare}
-        iconColor="text-violet-500"
-        bgColor="bg-violet-500/10"
+        title="Music Generation"
+        description="Turn your prompt into music"
+        icon={Music}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
       />
       <div className="px-4 lg:px-8">
         <div>
@@ -79,7 +70,7 @@ const ConversationPage = () => {
                       focus-visible:ring 
                       focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="What is your question?"
+                        placeholder="Choose a music to generate"
                         {...field}
                       />
                     </FormControl>
@@ -104,30 +95,20 @@ const ConversationPage = () => {
               <Loader />
             </div>
           )}
-          {messages.length === 0 && !isLoading && (
+          {!music && !isLoading && (
             <div>
-              <Empty label="No conversation started." />
+              <Empty label="No music started." />
             </div>
           )}
-          <div className="flex flex-col-reverse gap-y-4">
-            {messages.map((message) => (
-              <div
-                key={message.content}
-                className={cn(
-                  "p-8 w-full items-start gap-x-8 rounded-lg",
-                  message.role === "user"
-                    ? "bg-white border border-black/10"
-                    : "bg-muted"
-                )}>
-                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content}</p>
-              </div>
-            ))}
-          </div>
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default ConversationPage;
+export default MusicPage;
--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -1,26 +1,22 @@
 "use client";
-import axios from "axios";
+import Empty from "@/components/empty";
 import Heading from "@/components/heading";
-import { MessageSquare } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { formSchema } from "./constants";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
+import Loader from "@/components/loader";
+import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Video } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { ChatCompletionRequestMessage } from "openai";
-import Empty from "@/components/empty";
-import Loader from "@/components/loader";
-import { cn } from "@/lib/utils";
-import UserAvatar from "@/components/user-avatar";
-import BotAvatar from "@/components/bot-avatar";
-const ConversationPage = () => {
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { formSchema } from "./constants";
+const VideoPage = () => {
   const router = useRouter();
 
-  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+  const [video, setVideo] = useState<string>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -33,17 +29,11 @@ const ConversationPage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const userMessage: ChatCompletionRequestMessage = {
-        role: "user",
-        content: values.prompt,
-      };
-      const newMessages = [...messages, userMessage];
+      setVideo(undefined);
 
-      const response = await axios.post("/api/conversation", {
-        messages: newMessages,
-      });
-      setMessages((current) => [...current, userMessage, response.data]);
+      const response = await axios.post("/api/video", values);
 
+      setVideo(response.data[0]);
       form.reset();
     } catch (error: any) {
       //todo
@@ -54,11 +44,11 @@ const ConversationPage = () => {
   return (
     <div>
       <Heading
-        title="Conversation"
-        description="Our most advanced conversation"
-        icon={MessageSquare}
-        iconColor="text-violet-500"
-        bgColor="bg-violet-500/10"
+        title="Video Generation"
+        description="Turn your prompt into video"
+        icon={Video}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
       />
       <div className="px-4 lg:px-8">
         <div>
@@ -79,7 +69,7 @@ const ConversationPage = () => {
                       focus-visible:ring 
                       focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="What is your question?"
+                        placeholder="Choose a video to generate"
                         {...field}
                       />
                     </FormControl>
@@ -104,30 +94,22 @@ const ConversationPage = () => {
               <Loader />
             </div>
           )}
-          {messages.length === 0 && !isLoading && (
+          {!video && !isLoading && (
             <div>
-              <Empty label="No conversation started." />
+              <Empty label="No video started." />
             </div>
           )}
-          <div className="flex flex-col-reverse gap-y-4">
-            {messages.map((message) => (
-              <div
-                key={message.content}
-                className={cn(
-                  "p-8 w-full items-start gap-x-8 rounded-lg",
-                  message.role === "user"
-                    ? "bg-white border border-black/10"
-                    : "bg-muted"
-                )}>
-                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content}</p>
-              </div>
-            ))}
-          </div>
+          {video && (
+            <video
+              className="w-full aspect-video mt-8 rounded-lg border bg-black"
+              controls>
+              <source src={video} />
+            </video>
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default ConversationPage;
+export default VideoPage;
